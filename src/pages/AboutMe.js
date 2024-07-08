33201fd83ce432e3740f209fa0f76fac02782fba
Sai Chaparala <cskrishna9306@gmail.com>
2024-07-08 16:46:03 +0400

    capitol.jpeg -> capitol.jpg

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../css/AboutMe.css'
-import capitol from '../images/capitol.jpeg'
+import capitol from '../images/capitol.jpg'
 
 function AboutMe() {
   return (
     <div className='about-me'>
         <h1>About Me</h1>
         <p>
-          Hello! I'm a recent graduate from the University of Wisconsin - Madison, currently pursuing my Master's in Computer Science and Engineering at the University of California - San Diego. 
+          Hello! I'm a recent graduate from the <b>University of Wisconsin - Madison</b>, currently pursuing my Master's in Computer Science and Engineering at the <b>University of California - San Diego</b>. 
           I’m an enthusiastic tech savvy who is inspired by the rapid growth of modern-day technology. 
-          In particular, I'm intrigued by the variety of services offered by various cloud providers with my passion lying in cloud infrastructure.
+          In particular, I'm intrigued by the variety of services offered by various cloud providers with my passion lying in <b>cloud infrastructure</b>.
         </p>
         <p>
           I'm actively seeking internships in this field for Summer 2025 to gain hands-on experience and contribute to cutting-edge projects. 

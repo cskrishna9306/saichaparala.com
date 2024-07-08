7ec171dce5f747485f5db18ca33b05e5eb5d4575
Sai Chaparala <cskrishna9306@gmail.com>
2024-07-08 16:40:31 +0400

    Cleaning up About Me code

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,18 +6,16 @@ function AboutMe() {
   return (
     <div className='about-me'>
         <h1>About Me</h1>
-        <p>Hello! I'm a recent graduate from the University of Wisconsin - Madison, currently pursuing my Master's in Computer Science and Engineering at the University of California - San Diego. 
+        <p>
+          Hello! I'm a recent graduate from the University of Wisconsin - Madison, currently pursuing my Master's in Computer Science and Engineering at the University of California - San Diego. 
           I’m an enthusiastic tech savvy who is inspired by the rapid growth of modern-day technology. 
-          In particular, I'm intrigued by the variety of services offered by various cloud providers with my passion lying in cloud infrastructure</p>
-        <p>I'm actively seeking internships in this field for Summer 2025 to gain hands-on experience and contribute to cutting-edge projects. 
-          With a solid foundation in software development, database management, and cloud architecture, I'm eager to leverage my skills and knowledge to drive impactful solutions in the tech industry.</p>
-        {/* <div className="row">
-          <div className='column'>
-            <img src={image1} alt='pic'/>
-            <img src={image2} alt='pic'/>
-          </div> */}
-          <img src={capitol} alt='pic' />
-        {/* </div> */}
+          In particular, I'm intrigued by the variety of services offered by various cloud providers with my passion lying in cloud infrastructure.
+        </p>
+        <p>
+          I'm actively seeking internships in this field for Summer 2025 to gain hands-on experience and contribute to cutting-edge projects. 
+          With a solid foundation in software development, database management, and cloud architecture, I'm eager to leverage my skills and knowledge to drive impactful solutions in the tech industry.
+        </p>
+        <img src={capitol} alt='pic' />
     </div>
   )
 }

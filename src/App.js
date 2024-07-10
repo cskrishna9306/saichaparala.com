7f3a776486293469e069e8940227e473bb06d82e
Sai Chaparala <cskrishna9306@gmail.com>
2024-07-10 17:35:12 +0400

    Removed the Music page and replaced it with Skills.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Interests from './pages/Interests';
-import Music from './pages/Music'
+import Skills from './pages/Skills'
 import Projects from './pages/Projects'
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/aboutMe' element={<AboutMe />} />
         <Route path='/interests' element={<Interests />} />
-        <Route path='/music' element={<Music />} />
+        <Route path='/skills' element={<Skills />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
     </Router>
---
+++ src/pages/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Skills.css'
+
+function Skills() {
+  return (
+    <div className='music'>
+      <h1>Skills</h1>
+
+    </div>
+  );
+}
+
+export default Skills;

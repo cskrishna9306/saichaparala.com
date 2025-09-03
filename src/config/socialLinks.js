f81329c416d73d2a5a84f887eedc51e5d91d24da
Sai Chaparala <cskrishna9306@gmail.com>
2025-09-03 15:30:35 -0700

    feat(config): Maintain a config file to all my socials

---
+++ src/config/socialLinks.js
@@ -0,0 +1,8 @@
+// Social media links configuration
+export const socialLinks = {
+  email: "mailto:cskrishna9306@gmail.com",
+  instagram: "https://www.instagram.com/_saaiii/",
+  linkedin: "https://www.linkedin.com/in/sai-chaparala-71817b206",
+  github: "https://github.com/cskrishna9306",
+  resume: "https://sai-chaparala-resume.s3.amazonaws.com/resume.pdf"
+};

38aed0c5d5d4c0961e28b66ab3647a55a0cb7de7
Sai Chaparala <cskrishna9306@gmail.com>
2025-09-03 17:56:53 -0700

    feat(skills): Completed skills page POC

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -16,6 +16,8 @@ import postgresql from '../images/technology/postgresql.svg'
 import postman from '../images/technology/postman.svg'
 import react from '../images/technology/react.svg'
 import aws_saa from '../images/technology/aws-saa.png'
+import aws_logo from '../images/technology/aws-logo.jpeg'
+import gcp_logo from '../images/technology/gcp-logo.png'
 import apple from '../images/technology/apple.svg'
 import windows from '../images/technology/windows11.svg'
 import linux from '../images/technology/linux.svg'
@@ -23,62 +25,129 @@ import linux from '../images/technology/linux.svg'
 function Skills() {
   return (
     <div className='skills'>
-      <h1>Skills</h1>
-      <ul>
-        <li>
+      <h1>Technologies</h1>
+      
+      <div className="skills-grid">
+        <div className="skill-category">
           <h2>Frontend</h2>
-          <div className='container'>
-              {/* <video src={html}></video> */}
-              {/* <video playsinline autoplay loop style={{"max-height": "99%", "max-width": "99%", "min-width": "auto"}} src={html}></video> */}
-              <img src={html}></img>
-              <img src={css}></img>
-              <img src={javascript}></img>
-              <img src={dart}></img>
+          <div className="technologies">
+            <div className="tech-item">
+              <img src={html} alt="HTML5" />
+              <span>HTML5</span>
+            </div>
+            <div className="tech-item">
+              <img src={css} alt="CSS3" />
+              <span>CSS3</span>
+            </div>
+            <div className="tech-item">
+              <img src={javascript} alt="JavaScript" />
+              <span>JavaScript</span>
+            </div>
+            <div className="tech-item">
+              <img src={dart} alt="Dart" />
+              <span>Dart</span>
+            </div>
           </div>
-          {/* <div className='container'>
-            <img src={dart}></img>
-          </div> */}
-        </li>
-        <li>
+        </div>
+
+        <div className="skill-category">
           <h2>Backend</h2>
-          <div className='container'>
-            <img src={c}></img>
-            <img src={cpp}></img>
-            <img src={java}></img>
+          <div className="technologies">
+            <div className="tech-item">
+              <img src={c} alt="C" />
+              <span>C</span>
+            </div>
+            <div className="tech-item">
+              <img src={cpp} alt="C++" />
+              <span>C++</span>
+            </div>
+            <div className="tech-item">
+              <img src={java} alt="Java" />
+              <span>Java</span>
+            </div>
+            <div className="tech-item">
+              <img src={python} alt="Python" />
+              <span>Python</span>
+            </div>
+            <div className="tech-item">
+              <img src={bash} alt="Bash" />
+              <span>Bash</span>
+            </div>
           </div>
-          <div className='container'>
-          <img src={python}></img>
-          <img src={bash}></img>
-          </div>
-        </li>
-        <li>
+        </div>
+
+        <div className="skill-category">
           <h2>Frameworks & Tools</h2>
-          <div className='container'>
-            <img src={flutter}></img>
-            <img src={react}></img>
-            <img src={docker}></img>
+          <div className="technologies">
+            <div className="tech-item">
+              <img src={flutter} alt="Flutter" />
+              <span>Flutter</span>
+            </div>
+            <div className="tech-item">
+              <img src={react} alt="React" />
+              <span>React</span>
+            </div>
+            <div className="tech-item">
+              <img src={docker} alt="Docker" />
+              <span>Docker</span>
+            </div>
+            <div className="tech-item">
+              <img src={mysql} alt="MySQL" />
+              <span>MySQL</span>
+            </div>
+            <div className="tech-item">
+              <img src={postgresql} alt="PostgreSQL" />
+              <span>PostgreSQL</span>
+            </div>
+            <div className="tech-item">
+              <img src={postman} alt="Postman" />
+              <span>Postman</span>
+            </div>
           </div>
-          <div className='container'>
-            <img src={mysql}></img>
-            <img src={postgresql}></img>
-            <img src={postman}></img>
+        </div>
+
+        <div className="skill-category">
+          <h2>Hyperscalers</h2>
+          <div className="technologies">
+            <div className="tech-item">
+              <img src={aws_logo} alt="AWS" />
+              <span>AWS</span>
+            </div>
+            <div className="tech-item">
+              <img src={gcp_logo} alt="Google Cloud Platform" />
+              <span>Google Cloud</span>
+            </div>
           </div>
-        </li>
-        <li>
+        </div>
+
+        <div className="skill-category">
           <h2>Certifications</h2>
-          <div className='container'>
-            <img src={aws_saa}></img>
+          <div className="technologies">
+            <div className="tech-item">
+              <img src={aws_saa} alt="AWS SAA" />
+              <span>AWS SAA</span>
+            </div>
           </div>
-        </li>
-        <li>
+        </div>
+
+        <div className="skill-category">
           <h2>Operating Systems</h2>
-          <div className='container'>
-            <img src={apple} style={{background: "white", borderRadius: "10%"}}></img>
-            <img src={windows}></img>
-            <img src={linux}></img>
+          <div className="technologies">
+            <div className="tech-item">
+              <img src={apple} alt="macOS" style={{background: "white", borderRadius: "10%"}} />
+              <span>macOS</span>
+            </div>
+            <div className="tech-item">
+              <img src={windows} alt="Windows" />
+              <span>Windows</span>
+            </div>
+            <div className="tech-item">
+              <img src={linux} alt="Linux" />
+              <span>Linux</span>
+            </div>
           </div>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   );
 }

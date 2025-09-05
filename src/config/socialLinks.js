a7a089fcc73003fb5e9f00e2631db2c65ec59fc9
Sai Chaparala <cskrishna9306@gmail.com>
2025-09-05 16:46:38 -0700

    Manage links directly within the file

--- src/config/socialLinks.js
+++
@@ -1,8 +0,0 @@
-// Social media links configuration
-export const socialLinks = {
-  email: "mailto:cskrishna9306@gmail.com",
-  instagram: "https://www.instagram.com/_saaiii/",
-  linkedin: "https://www.linkedin.com/in/sai-chaparala-71817b206",
-  github: "https://github.com/cskrishna9306",
-  resume: "https://sai-chaparala-resume.s3.amazonaws.com/resume.pdf"
-};

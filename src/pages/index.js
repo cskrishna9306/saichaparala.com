47ce8d34a28cc30f0ff7c6e5f8191b96e0712659
Sai Chaparala <cskrishna9306@gmail.com>
2025-09-05 16:46:14 -0700

    feat(pages): Add index.js to export the React page as a package

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+// Export all page components
+export { default as Home } from './Home';
+export { default as AboutMe } from './AboutMe';
+export { default as Interests } from './Interests';
+export { default as Skills } from './Skills';
+export { default as Projects } from './Projects';

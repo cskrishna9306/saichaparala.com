cfd396dc8a9efd73e842d29df047194d58a6e73a
Sai Chaparala <79293787+cskrishna9306@users.noreply.github.com>
2026-05-31 16:25:03 -0500

    feat(navbar): Added tooltips to each navbar link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Navbar.css';
 import { portraits } from './images';
@@ -7,31 +7,46 @@ import { portraits } from './images';
 const socialLinks = {
   email: {
     url: "mailto:cskrishna9306@gmail.com",
-    icon: "fa fa-envelope"
+    icon: "fa fa-envelope",
+    tooltip: "Send an email to me"
   },
   instagram: {
     url: "https://www.instagram.com/_saaiii/",
-    icon: "fab fa-instagram"
+    icon: "fab fa-instagram",
+    tooltip: "Follow me on Instagram"
   },
   linkedin: {
     url: "https://www.linkedin.com/in/sai-chaparala-71817b206",
-    icon: "fab fa-linkedin-in"
+    icon: "fab fa-linkedin-in",
+    tooltip: "Connect with me on LinkedIn"
   },
   github: {
     url: "https://github.com/cskrishna9306",
-    icon: "fab fa-github"
+    icon: "fab fa-github",
+    tooltip: "Check out my GitHub"
   },
   resume: {
     url: "https://sai-chaparala-resume.s3.amazonaws.com/resume.pdf",
-    icon: "fa fa-file-pdf"
+    icon: "fa fa-file-pdf",
+    tooltip: "View my resume"
   }
 };
+
+const navLinks = [
+  { path: "/", label: "/", tooltip: "Home" },
+  { path: "/aboutMe", label: "whoami", tooltip: "About me" },
+  { path: "/interests", label: "interests.txt", tooltip: "Interests" },
+  { path: "/skills", label: "lib", tooltip: "Skills" },
+  { path: "/projects", label: "git", tooltip: "Projects" }
+];
 
 function Navbar() {
   // Initialize a variable "click" that stores the current state value and its corresponding setter function "setClick" that allows us to update the "click" state variable
   const [click, setClick] = useState(false);
+  
   // Handles the state value for the "click" variable: true -> false, false -> true
-  const handleClick = () => setClick(!click);
+  // const handleClick = () => setClick(!click);
+  
   // Sets the state of the "click" variable to be false
   const closeMobileMenu = () => setClick(false);
 
@@ -57,44 +72,35 @@ function Navbar() {
           {/* Links to different pages */}
           <ul className={click ? 'navbar-menu active' : 'navbar-menu'}>
           {/* <ul className='nav-menu'> */}
-            
-            {/* Navbar link to the Home page */}
-            <Link to='/' style={{textDecoration: 'none'}} onClick={closeMobileMenu}>
-              <li>
-                /
-              </li>
-            </Link>
-            {/* Navbar link to the AboutMe.js page */}
-            <Link to='/aboutMe' style={{textDecoration: 'none'}} onClick={closeMobileMenu}>
-              <li>
-                whoami
-              </li>
-            </Link>
-            {/* Navbar link to the Interests.js page */}
-            <Link to='/interests' style={{textDecoration: 'none'}} onClick={closeMobileMenu}>
-              <li>
-                interests.txt
-              </li>
-            </Link>
-            {/* Navbar link to the Skills.js page */}
-            <Link to='/skills' style={{textDecoration: 'none'}} onClick={closeMobileMenu}>
-              <li>
-                lib
-              </li>
-            </Link>
-            {/* Navbar link to the Projects.js page */}
-            <Link to='/projects' style={{textDecoration: 'none'}} onClick={closeMobileMenu}>
-              <li>
-                git
-              </li>
-            </Link>
+            {navLinks.map(({ path, label, tooltip }) => (
+              <Link
+                key={path}
+                to={path}
+                className='tooltip-target'
+                data-tooltip={tooltip}
+                style={{ textDecoration: 'none' }}
+                onClick={closeMobileMenu}
+                aria-label={tooltip}
+              >
+                <li>
+                  {label}
+                </li>
+              </Link>
+            ))}
           </ul>
 
           {/* Social Handles in Navigation bar*/}
             <ul className='navbar-socials'>
-              {Object.entries(socialLinks).map(([key, { url, icon }]) => (
+              {Object.entries(socialLinks).map(([key, { url, icon, tooltip }]) => (
                 <li key={key}>
-                  <a href={url} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className='tooltip-target'
+                    data-tooltip={tooltip}
+                    aria-label={tooltip}
+                  >
                     <i className={icon}></i>
                   </a>
                 </li>
